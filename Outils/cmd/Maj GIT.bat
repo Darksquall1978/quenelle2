999f3d2e1b2fb101a1d06b350dde50194c88afe4
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2021-11-27 15:11:38 +0100

    Add files via upload

---
+++ Outils/cmd/Maj GIT.bat
@@ -0,0 +1,28 @@
+@echo off
+Color 0e & Mode con cols=100 lines=40
+
+git remote add wow git://github.com/Darksquall1978/quenelle2.git
+
+
+
+git fetch wow
+
+git fetch
+
+git pull
+
+::@echo Mise a jour OK
+timeout /t 3 /nobreak > nul
+
+::RMDIR /s /q quenelle
+
+::modif.bat
+
+cd..
+cd..
+::RMDIR /s /q Git
+
+::Launcher.exe
+exit
+
+
