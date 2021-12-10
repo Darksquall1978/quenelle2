06e69ea21737d127c257787d2db4228a8e637730
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2021-12-10 17:42:31 +0100

    Add files via upload

---
+++ Outils/reamlist/couisine.bat
@@ -0,0 +1,20 @@
+
+ECHO Ab-Irato
+cd..
+cd..
+ECHO 2 = Ab-Irato
+
+:2
+ECHO.
+Color 0e & Mode con cols=51 lines=4
+
+::ECHO la femme a la couisine
+@echo off
+::ECHO.
+DEL Data\frFR\realmlist.wtf
+ECHO set realmlist 26.0.12.25 >>Data/frFR/realmlist.wtf
+ECHO Connection au Royaume de la femme a la couisine...
+timeout /t 3 /nobreak > nul
+Color 0a
+::ECHO Le jeu demarrera dans 3 secondes...
+::timeout /t 3 /nobreak > nul
