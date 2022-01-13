f4d49524b4361d4fd97e097577c277d57b0d4b11
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-01-13 20:13:27 +0100

    Add files via upload

---
+++ Outils/Addons/abiratobots.bat
@@ -0,0 +1,11 @@
+
+cd..
+cd..
+
+xcopy /s /y \\26.0.12.25\Ab-Irato\Addons\AbIrato-Bots Interface\AddOns\AbIrato-Bots\ 
+
+
+
+
+
+exit
---
+++ Outils/Addons/bagnon.bat
@@ -0,0 +1,15 @@
+
+cd..
+cd..
+
+xcopy /s /y \\26.0.12.25\Ab-Irato\Addons\bagnon Interface\AddOns\Bagnon\
+xcopy /s /y \\26.0.12.25\Ab-Irato\Addons\bagnon_config Interface\AddOns\Bagnon_Config\
+xcopy /s /y \\26.0.12.25\Ab-Irato\Addons\bagnon_forever Interface\AddOns\Bagnon_Forever\
+xcopy /s /y \\26.0.12.25\Ab-Irato\Addons\bagnon_guildbank Interface\AddOns\Bagnon_Guildbank\
+xcopy /s /y \\26.0.12.25\Ab-Irato\Addons\bagnon_tooltips Interface\AddOns\Bagnon_Tooltips\
+
+
+
+
+
+exit
